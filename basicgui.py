a4b64d5b8072e3b17d980ca3dca045156eaeb25c
Meta Jungprasert <meta.jps@gmail.com>
2025-05-02 12:50:36 +0700

    add calcurator function

--- basicgui.py
+++ basicgui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk, messagebox
 
 GUI = Tk()
 GUI.title('Hello World')
@@ -9,5 +10,21 @@ L1.pack()
 
 L2 = Label(GUI, text='Hello Roger', font=(None, 20))
 L2.pack()
+
+v1 = StringVar()
+E1 = ttk.Entry(GUI, textvariable=v1)
+E1.pack()
+
+v2 = StringVar()
+E2 = ttk.Entry(GUI, textvariable=v2)
+E2.pack()
+
+def cal():
+    print(c := float(v1.get()) * float(v2.get()))
+    text = f'{v1.get()}x{v2.get()}={c}'
+    messagebox.showinfo('Result', text)
+
+b1 = ttk.Button(GUI, text='Calcurate Now', command=cal)
+b1.pack()
 
 GUI.mainloop()
