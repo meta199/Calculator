ee6b71a772066d59e4482fa65119944cea656b0f
Meta Jungprasert <meta.jps@gmail.com>
2025-05-02 11:31:37 +0700

    add Label

--- basicgui.py
+++ basicgui.py
@@ -4,7 +4,10 @@ GUI = Tk()
 GUI.title('Hello World')
 GUI.geometry('500x500')
 
-L1 = Label(GUI, text='Hello World', font=(None, 20))
+L1 = Label(GUI, text='Hello Meta', font=(None, 20))
 L1.pack()
+
+L2 = Label(GUI, text='Hello Roger', font=(None, 20))
+L2.pack()
 
 GUI.mainloop()
